2ca255ce38c8c1948985cf778f3285c2e6bb89d6
Rene <rene.bergeron@gmail.com>
2016-11-03 20:07:11 +0100

    [tech] First commit to perso.

---
+++ FirstApp/Accueil.swift
@@ -0,0 +1,52 @@
+//
+//  Accueil.swift
+//  FirstApp
+//
+//  Created by Rene on 02/11/2016.
+//  Copyright © 2016 Rene. All rights reserved.
+//
+
+import UIKit
+
+class Acccueil: UIViewController {
+    
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        // TODO 
+        navigationController?.navigationBar.tintColor = UIColor.green
+        
+        
+        navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.red]
+        
+        navigationController?.navigationBar.barTintColor = UIColor.yellow
+
+        
+        
+//        navigationItem.backBarButtonItem?.title = "Maison"
+
+        
+        // On change bouton back ici sinon il est instancié sur la page
+        navigationItem.backBarButtonItem = UIBarButtonItem(title:"Maison", style: .plain, target:nil, action:nil)
+
+    }
+    
+    
+    @IBAction func clickSourceButton(sender : UIButton) {
+        
+        let storyboard = UIStoryboard(name : "Main", bundle : nil)
+        
+        let controller = storyboard.instantiateViewController(withIdentifier: "FirstApp") as! FirstApp
+        
+        controller.niveau = sender.tag
+        
+        self.navigationController?.show(controller, sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let backItem = UIBarButtonItem()
+        backItem.title = "What?????"
+        navigationItem.backBarButtonItem = backItem
+    }
+    
+}
